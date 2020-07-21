3ca9673386da50c0c8c189fad7a0fd582451fa3f
Andres Ronquillo <ronkio91@gmail.com>
2020-07-20 23:18:29 -0600

    clase operadores logicos

--- Javascript/Hola_Mundo/js/06_condicionales.js
+++ Javascript/Hola_Mundo/js/06_condicionales.js
@@ -44,3 +44,29 @@ if (edad1 >= 18) {
 
 
 }
+
+/* operadores logicos
+AND (Y): &&
+OR (O):||
+Negacion : !
+*/
+
+//negacion
+var year = 2028;
+
+if (year != 2016) {
+    console.log(" El anio no es 2016 realmente es" + year);
+}
+// AND
+if (year >= 2000 && year <= 2020 && year != 2018) {
+    console.log("Estamos en la era moderna")
+} else {
+    console.log("Estamos en la era post moderna")
+}
+// Or
+if (year == 2008 || (year >= 2018 && year == 2028)) {
+    console.log("El anio acaba en 8")
+
+} else {
+    console.log("Anio no Registrado")
+}
